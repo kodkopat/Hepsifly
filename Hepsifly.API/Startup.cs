6295bac409ea065217946aa51409692b4959e5b3
Yaşar ARAZ <44663562+kodkopat@users.noreply.github.com>
2025-06-10 18:11:26 +0300

    Update Program and Startup for .NET 9

--- Hepsifly.API/Startup.cs
+++ Hepsifly.API/Startup.cs
@@ -1,17 +1,8 @@
 using Hepsifly.Infrastructure.Dependencies;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
 using Hepsifly.Core;
 
 namespace Hepsifly.API
@@ -22,15 +13,17 @@ namespace Hepsifly.API
         {
             Configuration = configuration;
         }
+
         public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers().AddNewtonsoftJson();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hepsifly.API", Version = "v1" });
             });
+
             services.AddAppDependencies();
             services.AddMemoryCache();
             services.AddStackExchangeRedisCache(c =>
@@ -39,20 +32,20 @@ namespace Hepsifly.API
                 c.InstanceName = "master";
             });
         }
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+
+        public void Configure(WebApplication app)
         {
-            if (env.IsDevelopment())
+            if (app.Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hepsifly.API v1"));
             }
+
             app.UseRouting();
             app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+
+            app.MapControllers();
         }
     }
 }
